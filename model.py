d6d07b8cb39c3d06a83b9ee6ff2defd9be6541ee
KenroyDytrim <131690187+KenroyDytrim@users.noreply.github.com>
2024-06-20 19:53:03 +0700

    Update model.py

--- model.py
+++ model.py
@@ -47,7 +47,7 @@ def analysis(*args, **kwargs):
   test=pd.DataFrame(columns= namet)
   test.loc[ len(test.index )] =td
 
-  if test.iat[0, 9] == 'True'
+  if test.iat[0, 9] == 'True':
         test.iat[0, 9] = 1
   else:
         test.iat[0, 9] = 0
